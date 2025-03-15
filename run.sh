acc45b53ecf93e2c0491bc5259901bde88ffc376
adityasingh03rajput <123081746+adityasingh03rajput@users.noreply.github.com>
2025-03-15 12:46:39 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,5 +7,11 @@ sudo apt-get install -y libgl1-mesa-glx
 # Install Python dependencies
 pip install -r requirements.txt
 
+# Download YOLOv5 and custom-trained model
+git clone https://github.com/ultralytics/yolov5
+cd yolov5
+pip install -r requirements.txt
+cd ..
+
 # Run the Streamlit app
 streamlit run app.py
