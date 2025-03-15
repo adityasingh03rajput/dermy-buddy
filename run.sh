30f7188b6d95484add817f0e0f5783987a665b9e
adityasingh03rajput <123081746+adityasingh03rajput@users.noreply.github.com>
2025-03-15 12:38:26 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-# Install dependencies from requirements.txt
+# Install system dependencies (for libGL.so.1)
+sudo apt-get update
+sudo apt-get install -y libgl1-mesa-glx
+
+# Install Python dependencies
 pip install -r requirements.txt
 
 # Run the Streamlit app
